2253f806045325b5a915430e167429a76c77e1c2
Erlan Abdullaev <abdullaev.erlan.kk@gmail.com>
2022-09-30 11:02:20 +0600

    push master

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-let arr = [1, 2, 3, 1, 2, 4, 7, 6]
+// let arr = [1, 2, 3, 1, 2, 4, 7, 6]
 
-function dedude(arr) {
-    
-}
+// function dedude(arr) {
+//     let result = [];
+//     arr.forEach(element => {
+//         if (result.length === 0) {
+//             result.push(element);
+//         } else if (!result.includes(element)){
+//             result.push(element);
+//         }
+//     });
+//     return result;
+// }
+// console.log(dedude(arr));
+
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let result = arr.filter((number) => {
+//     return number % 2 === 0;
+// });
+// console.log(result);
+
+
+
+// let obj = {name: "Erlan", age: 45, position: undefined};
+// let keys = Object.keys(obj)
+// console.log(keys);
+
+
+
+
+const title = document.createElement("h1");
+title.innerText = "hello"
+document.body.appendChild(title)
