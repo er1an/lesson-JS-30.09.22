8ffb21c7f7c80796f5eed24a3e7601c31ad370b8
Erlan Abdullaev <89101313+er1an@users.noreply.github.com>
2022-10-03 10:52:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,14 @@
 
 
 
-const title = document.createElement("h1");
-title.innerText = "hello"
-document.body.appendChild(title)
+// const title = document.createElement("h1");
+// title.innerText = "hello"
+// document.body.appendChild(title)
+
+
+// таблица умножения
+//  for (let i = 1; i < 10; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
